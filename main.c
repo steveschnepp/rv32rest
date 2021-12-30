ac5451780de199e164e601c81d92c5c2472c5306
ZyuC <m9t1k3zczc@gmail.com>
2021-12-30 22:30:10 +0900

    CPUを作成，バイナリをメモリに保存

--- main.c
+++ main.c
@@ -1,9 +1,29 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-    //メモリ 1kB
-    uint32_t memory[256];
+#define ROM_SIZE 1024
+#define RAM_SIZE 1024
+#define MEM_SIZE ROM_SIZE + RAM_SIZE
+#define ROM_SIZE_32 (ROM_SIZE / sizeof(uint32_t))
+#define RAM_SIZE_32 (RAM_SIZE / sizeof(uint32_t))
+#define MEM_SIZE_32 (MEM_SIZE / sizeof(uint32_t))
+
+typedef struct {
+    uint32_t registers[32];
+    uint32_t memory[MEM_SIZE_32];
+} Cpu;
+
+void initCpu(Cpu* cpu) {
+    memset(cpu->registers, 0, sizeof(cpu->registers));
+    memset(cpu->memory, 0, sizeof(cpu->memory));
+    return;
+}
+
+int main(int argc, char* argv[]) {
+    Cpu cpu;
+    initCpu(&cpu);
 
     //バイナリファイルが指定されていない場合
     if (argc != 2) {
@@ -12,7 +32,7 @@ int main(int argc, char *argv[]) {
     }
 
     //バイナリファイルの読み込み
-    FILE *binary;
+    FILE* binary;
 
     binary = fopen(argv[1], "rb");
     if (binary == NULL) {
@@ -20,12 +40,12 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    fread(memory, sizeof(uint32_t), 128, binary);
+    fread(cpu.memory, sizeof(uint32_t), RAM_SIZE_32, binary);
     fclose(binary);
 
     //メモリダンプ
-    for (int i = 0; i < 128; i++) {
-        printf("0x%04x : 0x%08x\n", i * 4, memory[i]);
+    for (int i = 0; i < RAM_SIZE_32; i++) {
+        printf("0x%04x : 0x%08x\n", i * 4, cpu.memory[i]);
     }
 
     return 0;

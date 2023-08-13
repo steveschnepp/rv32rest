d0e52341e4e99d6457bca964b80e0ba5827931ec
Steve Schnepp <steve.schnepp@pwkf.org>
2023-08-13 17:55:55 +0200

    cpu register 'zero' abbreviated to z0

--- cpu.c
+++ cpu.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdio.h>
 
-char* register_name[] = {"zero", "ra", "sp",  "gp",  "tp", "t0", "t1", "t2",
+char* register_name[] = {"z0",   "ra", "sp",  "gp",  "tp", "t0", "t1", "t2",
                          "s0",   "s1", "a0",  "a1",  "a2", "a3", "a4", "a5",
                          "a6",   "a7", "s2",  "s3",  "s4", "s5", "s6", "s7",
                          "s8",   "s9", "s10", "s11", "t3", "t4", "t5", "t6"};

1f604266507cbff8a9d826c11564d077c012c4c1
Steve Schnepp <steve.schnepp@pwkf.org>
2023-08-14 00:03:51 +0200

    use 64bit read_cycles

--- sample/csr.c
+++ sample/csr.c
@@ -1,19 +1,26 @@
 #include "printf.h"
 
-unsigned long read_cycles() {
-    register unsigned long a0 asm ("a0") = 0;
-    asm volatile ("rdcycle a0"
-              : "+r" (a0)
+unsigned long long read_cycles() {
+	unsigned long high = 0;
+	unsigned long low = 0;
+
+    asm volatile ("rdcycle %0"
+              : "+r" (low)
               :
               : "memory");
-    return a0;
+    asm volatile ("rdcycleh %0"
+              : "+r" (high)
+              :
+              : "memory");
+    unsigned long long cycles = ((unsigned long long) high << 32) + low;
+    return cycles;
 }
 
 void main(void)
 {
 	for (int i = 0; i < 10; i ++) {
-		unsigned long cycles = read_cycles();
-		printf("%u ", cycles);
+		unsigned long long cycles = read_cycles();
+		printf("%llu ", cycles);
 	}
 
 }
